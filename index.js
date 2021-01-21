612ccb09895c183329ad0a80de8d2d52e6077d11
Omar Al Hennawi <omar504969@gmail.com>
2021-01-21 18:33:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,5 +11,14 @@ function divide(){
 
 }
 function add(a, b){
-  return (a + b)
+  return (a + b);
+}
+function subtract(a, b){
+  return (a - b);
+}
+function multiply(a, b){
+  return (a * b);
+}
+function divide(a, b){
+  return (a / b);
 }
